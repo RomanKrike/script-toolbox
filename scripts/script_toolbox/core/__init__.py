dbd21b55fcb4e3452665a580c0b5e3e5979e9acd
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-03 19:46:50 +0400

    Expose value API

--- scripts/script_toolbox/core/__init__.py
+++ scripts/script_toolbox/core/__init__.py
@@ -3,9 +3,17 @@
 from .config import load_config
 from .config import save_config
 from .executor import execute_script
+from .values import find_item
+from .values import get_value
+from .values import normalize_value
+from .values import store_value
 
 __all__ = [
     "load_config",
     "save_config",
     "execute_script",
+    "find_item",
+    "get_value",
+    "normalize_value",
+    "store_value",
 ]

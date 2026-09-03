be68e76090fd25158c6e80d7f671bb26759b1de4
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-03 19:38:20 +0400

    Add modular scaffold: scripts/script_toolbox/__init__.py

---
+++ scripts/script_toolbox/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+__version__ = "0.1.0-dev"
+
+
+def show():
+    from .bootstrap import show as _show
+    return _show()
+
+
+def reload_toolbox():
+    from .bootstrap import reload_toolbox as _reload_toolbox
+    return _reload_toolbox()

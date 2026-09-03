4cd6d0cdba80b82b23f373c67e7b5f9e4efc750b
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-03 22:46:30 +0400

    Expose active host and Nuke integration

--- scripts/script_toolbox/__init__.py
+++ scripts/script_toolbox/__init__.py
@@ -2,8 +2,10 @@
 from __future__ import print_function
 
 from .constants import PLUGIN_VERSION
+from .hosts import HOST
 
 __version__ = PLUGIN_VERSION
+__host__ = HOST.key
 
 
 def show():
@@ -19,3 +21,13 @@ def reload_toolbox():
 def hot_reload_toolbox():
     from .bootstrap import hot_reload_toolbox as _hot_reload_toolbox
     return _hot_reload_toolbox()
+
+
+def register_nuke_menu():
+    from .nuke_integration import register_menu
+    return register_menu()
+
+
+def register_nuke_panel():
+    from .nuke_integration import register_panel
+    return register_panel()

1d86ac30c9e3a9797ba527fc7d7d3f85f069a934
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-03 21:39:48 +0400

    Prepare Script Toolbox 0.2.5 hot update test

--- scripts/script_toolbox/constants.py
+++ scripts/script_toolbox/constants.py
@@ -2,7 +2,7 @@
 
 PACKAGE_NAME = "script_toolbox"
 DISPLAY_NAME = "Script Toolbox"
-PLUGIN_VERSION = "0.2.4"
+PLUGIN_VERSION = "0.2.5"
 
 WINDOW_OBJECT_NAME = "MayaScriptToolbox"
 EDITOR_OBJECT_NAME = "MayaScriptToolboxInterfaceEditor"

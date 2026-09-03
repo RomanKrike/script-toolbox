20befd028c6fd9940967586a6bcd6fdc638088dd
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-03 19:48:09 +0400

    Expose interface tree component

--- scripts/script_toolbox/ui/__init__.py
+++ scripts/script_toolbox/ui/__init__.py
@@ -2,6 +2,7 @@
 
 from .code_editor import CodeEditor
 from .code_editor import ScriptHighlighter
+from .interface_tree import ExistingInterfaceTree
 from .main_window import ScriptToolbox
 from .runtime import DisplayField
 from .runtime import RuntimeFolder
@@ -11,6 +12,7 @@ from .runtime import RuntimeFolderTabs
 __all__ = [
     "CodeEditor",
     "ScriptHighlighter",
+    "ExistingInterfaceTree",
     "ScriptToolbox",
     "DisplayField",
     "RuntimeFolder",

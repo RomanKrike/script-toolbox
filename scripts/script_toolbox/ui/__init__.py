33bce87f9c1122fa0a436b988d4d5f4363392a7e
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-08 22:35:47 +0400

    Install external scroll frames in interface editor

--- scripts/script_toolbox/ui/__init__.py
+++ scripts/script_toolbox/ui/__init__.py
@@ -10,6 +10,7 @@ from .editor_view_state import build_editor_view_state_class
 from .layout_editor_adapter import build_layout_editor_class
 from .layout_context import install_layout_property_context
 from .share_hooks import build_share_interface_editor_class
+from .editor_scroll_frames import build_scroll_frame_interface_editor_class
 from .interface_tree import ExistingInterfaceTree
 from .property_pane_style import install_property_pane_style
 from .properties.base import PropertyEditorBase
@@ -75,6 +76,9 @@ InterfaceEditor = build_editor_view_state_class(
     InterfaceEditor
 )
 InterfaceEditor = build_share_interface_editor_class(
+    InterfaceEditor
+)
+InterfaceEditor = build_scroll_frame_interface_editor_class(
     InterfaceEditor
 )
 install_property_pane_style(

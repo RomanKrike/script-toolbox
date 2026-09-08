5a6cd5926cd54978143932922e7fd4778e194e08
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-08 21:15:00 +0400

    Install expanding property script editor sizing

--- scripts/script_toolbox/ui/properties/__init__.py
+++ scripts/script_toolbox/ui/properties/__init__.py
@@ -4,9 +4,11 @@ from .registry import PROPERTY_EDITORS
 from .registry import create_editor
 from .registry import editor_class
 from .trigger_tabs import install_integrated_trigger_tabs
+from .script_editor_sizing import install_expanding_script_editors
 
 
 install_integrated_trigger_tabs()
+install_expanding_script_editors()
 
 
 __all__ = [

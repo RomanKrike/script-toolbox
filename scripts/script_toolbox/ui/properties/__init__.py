1c7ccc4fab57de3f89e9d2f80b57da1801b723d9
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-08 21:05:46 +0400

    Install integrated trigger add tab

--- scripts/script_toolbox/ui/properties/__init__.py
+++ scripts/script_toolbox/ui/properties/__init__.py
@@ -3,6 +3,11 @@
 from .registry import PROPERTY_EDITORS
 from .registry import create_editor
 from .registry import editor_class
+from .trigger_tabs import install_integrated_trigger_tabs
+
+
+install_integrated_trigger_tabs()
+
 
 __all__ = [
     "PROPERTY_EDITORS",

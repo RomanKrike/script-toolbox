5e0e4638bde876380282747b8242008b4f559b2a
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-09 14:20:31 +0400

    Include shared component styles

--- scripts/script_toolbox/style/__init__.py
+++ scripts/script_toolbox/style/__init__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from .builtin_icons import builtin_icon
+from .components import COMPONENT_STYLES
 from .icons import toolbar_icon as _legacy_toolbar_icon
 from .runtime_overrides import RUNTIME_OVERRIDES
 from .stylesheet import STYLE as BASE_STYLE
 
-STYLE = BASE_STYLE + RUNTIME_OVERRIDES
+STYLE = BASE_STYLE + COMPONENT_STYLES + RUNTIME_OVERRIDES
 
 
 def toolbar_icon(kind):

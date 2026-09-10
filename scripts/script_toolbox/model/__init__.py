581fd2f2d6fc707235ace0cde3809e901827ae9e
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-10 13:15:42 +0400

    Remove model compatibility installers

--- scripts/script_toolbox/model/__init__.py
+++ scripts/script_toolbox/model/__init__.py
@@ -1,22 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from . import items as _items
+from .index import DocumentIndex
 from .layouts import CONTAINER_KINDS
 from .layouts import LAYOUT_KINDS
 from .layouts import is_container_kind
 from .layouts import is_layout_kind
-from .toggle_button import install_toggle_button_kind
 
-
-# Install Toggle Button first because Toggle Icon deliberately chains on top of
-# its state-binding compatibility wrappers.
-install_toggle_button_kind()
-
-from .toggle_icon import install_toggle_icon_kind
-
-install_toggle_icon_kind()
-
-from .index import DocumentIndex
 
 create_item = _items.create_item
 normalize_document = _items.normalize_document

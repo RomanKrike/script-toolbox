1f037e8a715600c823969ff160154925328354bd
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-10 12:51:01 +0400

    Remove layout bootstrap side effects

--- scripts/script_toolbox/model/__init__.py
+++ scripts/script_toolbox/model/__init__.py
@@ -3,7 +3,6 @@
 from . import items as _items
 from .layouts import CONTAINER_KINDS
 from .layouts import LAYOUT_KINDS
-from .layouts import install_layout_kinds
 from .layouts import is_container_kind
 from .layouts import is_layout_kind
 from .toggle_button import install_toggle_button_kind
@@ -16,11 +15,7 @@ install_toggle_button_kind()
 from .toggle_icon import install_toggle_icon_kind
 
 install_toggle_icon_kind()
-install_layout_kinds()
 
-# Import after kind installation so DocumentIndex binds the extended
-# walk_items implementation that traverses Row / Column subtrees and sees the
-# dedicated stateful item kinds as first-class controls.
 from .index import DocumentIndex
 
 create_item = _items.create_item

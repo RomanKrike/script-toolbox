dded141879ba960dccdc3672232623cb8ed4ba1e
Roman Krike <61820604+RomanKrike@users.noreply.github.com>
2026-09-11 20:24:27 +0400

    Install trigger signature validation

--- scripts/script_toolbox/ui/properties/__init__.py
+++ scripts/script_toolbox/ui/properties/__init__.py
@@ -8,11 +8,13 @@ from .sections import set_property_available
 from .trigger_tabs import install_integrated_trigger_tabs
 from .script_editor_sizing import install_expanding_script_editors
 from .toggle_state_tabs import install_integrated_toggle_state_tabs
+from .trigger_validation import install_trigger_signature_validation
 
 
 install_integrated_trigger_tabs()
 install_expanding_script_editors()
 install_integrated_toggle_state_tabs()
+install_trigger_signature_validation()
 
 
 __all__ = [
